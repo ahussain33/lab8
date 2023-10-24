164b5b3691fc08ac0b9af23e5eba55135c636f16
ahussain33 <113541906+ahussain33@users.noreply.github.com>
2023-10-24 13:49:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 function init(){
- function myEventFunction(){
-  var textField = document.getElementById('entryinput');
-    alertVal = ("Abiha Hussain:" + textField.value);
-    alert(alertVal);
- }
-var button = document.getElementById('entrybutton');
-  button.addEventListener('click', myEventFunction());
+  var button = document.getElementById("entrybutton");
+  var input = document.getElementById("entryinput");
+  button.addEventListener("click", heading);
+  button.addEventListener("click", alert);
+ 
+ function heading() {
+    var output = document.getElementById("textoutput")
+    output.innerHTML = input.value;
+  }
+ 
+  function alert() {
+    text = ('Abiha Hussain: ' + input.value);
+    alert(text);
+  }
 }
 window.addEventListener('load', init);
